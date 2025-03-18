1de0ee777d32bc9d70147b44c45c8f5e277de897
ksenia <ksenia.pravdina15@gmail.com>
2025-03-18 13:04:04 +0100

    views, templates

--- evolution/simulation/urls.py
+++ evolution/simulation/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-
 from . import views
 
+app_name = 'simulation'
+
 urlpatterns = [
-    path("", views.index, name="index"),
+    path('', views.index, name='index'),
+    path('run/', views.run_simulation, name='run_simulation'),
 ]
--- evolution/simulation/views.py
+++ evolution/simulation/views.py
@@ -1,5 +1,34 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from io import StringIO
+import logging
+from .simulator import Simulation
+
+logger = logging.getLogger(__name__)
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, 'index.html')
+
+
+def run_simulation(request):
+    if request.method == 'POST':
+        size = int(request.POST['size'])
+        lifespan = int(request.POST['lifespan'])
+        fit_threshold = float(request.POST['fit_threshold'])
+        num_generations = int(request.POST['num_generations'])
+
+        log_stream = StringIO()
+        handler = logging.StreamHandler(log_stream)
+        logger.addHandler(handler)
+
+        sim = Simulation(size=size, lifespan=lifespan, fit_threshold=fit_threshold, num_generations=num_generations)
+        sim.run()
+
+        logs = log_stream.getvalue().split('\n')
+        logger.removeHandler(handler)
+
+        return render(request, 'output.html', {'logs': logs})
+    else:
+        return HttpResponseRedirect(reverse('simulation:index'))
